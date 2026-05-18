47704c3d10bfdc433a16a7db472cf4e31799a164
David Sebastian Duarte Rojas <147888454+RapteRPM@users.noreply.github.com>
2026-05-18 18:38:32 +0000

    cambios para despliegue de front

--- public/JS/api-url-config.js
+++ public/JS/api-url-config.js
@@ -2,6 +2,6 @@
 // Este archivo debe cargarse PRIMERO en todos los HTML
 
 // URL del backend en Codespaces
-window.API_URL = 'https://miniature-zebra-pjrxg4x6v7g52r565-3000.app.github.dev';
+window.API_URL = 'https://backend-production-05b9.up.railway.app'
 
 console.log('✅ API_URL configurada:', window.API_URL);
